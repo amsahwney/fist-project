89410440d8731a42aa10d3e341f3b36b27e310af
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-11-08 18:12:50 -0500

    i commented out the navbar and the outlet so i could test run the HEader element which still needs alot of work

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-
+import Header from './Components/Header'
 function App() {
 
   return (
     <div>
 
-      <Navbar /> 
+      {/* <Navbar />  */}
 
-      <Outlet />
+      {/* The dogs are showing up way to big and the hovering for the emotions isnt working properly right now. */}
+      <Header></Header>
+      {/* TESTTEST Delete Header when we route properly and put navbar and outlet back */}
+      
+      {/* <Outlet /> */}
 
       <footer> created by Amrita, Joyce, and Justin - add a heart here </footer>
 
