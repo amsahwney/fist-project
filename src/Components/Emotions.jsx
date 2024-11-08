309f5e691ff83a4d8b659d9e28d0247c28f6eb35
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-11-08 18:05:27 -0500

    tons of export and importing defaults for main to render

--- src/Components/Emotions.jsx
+++ src/Components/Emotions.jsx
@@ -1,16 +1,19 @@
 import { getValidToken } from './spotifyAPI/tokenManager';
 
+function Emotions () {
 
+    async function fetchSpotifyData() {
+        const token = await getValidToken();
 
-async function fetchSpotifyData() {
-    const token = await getValidToken();
+        const response = await fetch(`https://api.spotify.com/v1/playlists/${playlist.id}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
 
-    const response = await fetch(`https://api.spotify.com/v1/playlists/${playlist.id}`, {
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    });
-
-    const data = await response.json();
-    return data;
+        const data = await response.json();
+        return data;
+    }
 }
+
+export default Emotions 
