f8aab18ed4614f21b4227f66003e06eada8e67e6
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-11-10 15:53:53 -0500

    small edits and comments on the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,14 @@ function App() {
         <Link to="/">Home</Link>
         <Link to="/feedback">Feedback</Link>
       </Navbar>
-      <Routes>
+
+      {/* i dont think we need the below routes cause we routed everything with main js */}
+      {/* <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/feedback" element={<Feedback />} />
-      </Routes>
+      </Routes> */}
+
+      
       {/* Feedback page isnt working properly, homepage displays are also in feedbackpage */}
    
       {/* The dogs are showing up way to big and the hovering for the emotions isnt working properly right now. */}
