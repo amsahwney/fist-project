048fe1e91fcfb1411544126267a98f94e96d16c8
Joyce Sheng <joycesheng96@hotmail.com>
2024-11-12 11:59:13 -0500

    changed font, added image

--- src/Components/Feedback.jsx
+++ src/Components/Feedback.jsx
@@ -1,15 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import "./Feedback.css"
 
 function Feedback() {
   const [feedback, setFeedback] = useState("")
+  const [comments, setComment] = useState(() => {
+    const savedComments = localStorage.getItem("comments")
+    return savedComments ? JSON.parse(savedComments) : []
+  })
+
+  useEffect(() => {
+    localStorage.setItem("comments", JSON.stringify(comments))
+  }, [comments])
 
   const handleSubmit = (event) => {
     event.preventDefault()
+    if (comments.trim()) {
+      setComment([{id: Date.now(), text: comments}, ...comments])
+      setFeedback("") // Clear input
+    }
   }
-
+  
   return (
+    <div className="feedback-background">
     <div style={{padding:"20px"}}>
-      <h2>Feedback Page</h2>
+      <div className="feedback">
+      <h2>Tell us what you think!</h2>
       <form onSubmit={handleSubmit}>
         <textarea
         style={{width: "100%", height: "100px", marginBottom: "10px"}}
@@ -19,6 +34,15 @@ function Feedback() {
     </textarea>
         <button type="submit">Submit</button>
       </form>
+      <div className="comment-list">
+        {comments.map(({id, text}) => (
+          <div key={id} className="comment-list">
+            <p>{text}</p>
+      </div>
+    ))}
+      </div>
+      </div>
+      </div>
     </div>
   )
 }
