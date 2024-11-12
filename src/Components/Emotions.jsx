c68454bac10eb383bf74ba67feb249b9b91b8d06
Amrita <amritak.saw@gmail.com>
2024-11-12 01:15:50 -0500

    all elements are rendering on emotion page. weather card needs more styling

--- src/Components/Emotions.jsx
+++ src/Components/Emotions.jsx
@@ -1,16 +1,20 @@
 import { getValidToken } from './spotifyAPI/tokenManager'
 import { useState, useEffect } from 'react'
 import { pageDataArray as pageData } from './spotifyAPI/page-array'
+import { exerciseArray as exercises } from '../exercise-array'
 import { useParams } from 'react-router-dom'
+import weatherIcon from '../assets/weather-icon.svg';
 import "./emotions.css" 
 
 
 function Emotions () {
     const [emotionPlaylist, setEmotionPlaylist] = useState( [] )
     const [playlistImage, setPlaylistImage] = useState( [] )
+    const [weatherData, setWeatherData] = useState( {} )
     const {name: emotion} = useParams()
 
     const selectPlaylist = pageData.find(page => page.name === emotion)
+    const selectExercise = exercises.find(exercise => exercise.emotion === emotion)
 
     // begin playlist fetch
     async function fetchSpotifyTrackData() {
@@ -36,11 +40,37 @@ function Emotions () {
         setPlaylistImage(data.images) 
     }
 
+    async function fetchWeatherData(){
+        const weatherKey = import.meta.env.VITE_WEATHER_KEY
 
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(async (position) => {
+                const lat = position.coords.latitude
+                const lon = position.coords.longitude
+
+                try{
+                    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherKey}&units=imperial`)
+                    const data = await response.json();
+                    setWeatherData(data)
+                    console.log(weatherData)
+
+                } catch (error) {
+                    console.error("Error fetching weather data:", error)
+                }
+
+            }, (error) => {
+                console.error("Error getting location:", error)
+            })
+
+        } else {
+            console.error("Geolocation is not supported by this browser")
+        }  
+    }
 
     useEffect(()=>{
         fetchSpotifyPlaylistData()
         fetchSpotifyTrackData()
+        fetchWeatherData()
     }, [selectPlaylist])
 
     // begin page display return
@@ -51,6 +81,8 @@ function Emotions () {
         backgroundRepeat: 'no-repeat',
         width: '100vw',        
         height: '100vh',}}>
+
+        <h1 className='page-title'> when you're feeling {selectPlaylist.name}...</h1>
 
         <div className="playlist-container">  
             {playlistImage.length && (
@@ -81,6 +113,36 @@ function Emotions () {
                 ))}
             </ul>
         </div>
+
+      <div className='wellness'>
+        <h2>a breathing exercise</h2>
+        <ul>
+          <li>
+            <h3>{selectExercise.name}</h3>
+            <p>{selectExercise.description}</p>
+            <p>Inhale: {selectExercise.inhale}s, Hold: {selectExercise.hold}s, Exhale: {selectExercise.exhale}s</p>
+            <p>Cycles: {selectExercise.cycles}</p>
+          </li>
+        </ul>
+      </div>
+
+ 
+      <div className='weather-card'> 
+        <h2>consider {selectPlaylist.consider}</h2>
+        <img src={weatherIcon} />
+        <ul>
+            <li>
+            {weatherData.name && (
+                <div className='weather-report'>
+                <h3>Weather in {weatherData.name}:</h3>
+                <h3>{weatherData.weather[0].description}</h3>
+                <p> Temp: {weatherData.main.temp} °F </p> 
+                <p> Feels like: {weatherData.main.feels_like} °F</p>
+                </div>
+            )}
+            </li>
+        </ul>
+      </div>
         
         </div>
       )
---
+++ src/exercise-array.jsx
@@ -0,0 +1,52 @@
+export const exerciseArray = [
+      {
+        id: 1,
+        name: "box breathing",
+        description: "Inhale, hold, exhale, and hold each for 4 seconds.",
+        inhale: 4,
+        hold: 4,
+        exhale: 4,
+        cycles: 5,
+        emotion:"proud"
+      },
+      {
+        id: 2,
+        name: "4-7-8 breathing",
+        description: "Inhale for 4, hold for 7, and exhale for 8 seconds.",
+        inhale: 4,
+        hold: 7,
+        exhale: 8,
+        cycles: 5,
+        emotion:"tired"
+      },
+      {
+        id: 3,
+        name: "pursed lip breathing",
+        description: "Inhale for 5 seconds and exhale for 5 seconds with pursed lips.",
+        inhale: 5,
+        hold: 0,
+        exhale: 5,
+        cycles: 5,
+        emotion:"upset"
+      },
+      {
+        id: 4,
+        name: "diaphragmatic breathing",
+        description: "Deep breaths focusing on expanding the diaphragm. Inhale for 6 seconds, exhale for 6.",
+        inhale: 6,
+        hold: 0,
+        exhale: 6,
+        cycles: 5,
+        emotion:"stressed"
+      },
+      {
+        id: 5,
+        name: "resonant breathing",
+        description: "Inhale and exhale slowly, 5 seconds each.",
+        inhale: 5,
+        hold: 0,
+        exhale: 5,
+        cycles: 6,
+        emotion:"excited"
+      }
+    ]
