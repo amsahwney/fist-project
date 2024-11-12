b84cdf370e296a132530a7f37e18adf54a66014b
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-11-12 12:29:39 -0500

    dried out my code, deleted some comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import { Outlet, Routes, Route, Link } from 'react-router-dom'
@@ -12,18 +11,6 @@ function App() {
         <Link to="/feedback">Feedback</Link>
       </Navbar>
 
-      {/* i dont think we need the below routes cause we routed everything with main js */}
-      {/* <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/feedback" element={<Feedback />} />
-      </Routes> */}
-
-      
-      {/* Feedback page isnt working properly, homepage displays are also in feedbackpage */}
-   
-      {/* The dogs are showing up way to big and the hovering for the emotions isnt working properly right now. */}
-    
-      {/* TESTTEST Delete Header when we route properly and put navbar and outlet back */}
       
       <Outlet />
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,7 +9,7 @@ import tiredImg from '../assets/tired.svg';
 import tiredHoverImg from '../assets/tired-hover.svg';
 import upsetImg from '../assets/upset.svg';
 import upsetHoverImg from '../assets/upset-hover.svg';
-import "./header.css" 
+import './header.css';
 
 function Header() {
   const [emotionImages, setEmotionImages] = useState({
@@ -20,27 +20,20 @@ function Header() {
     upset: upsetImg,
   });
 
-  const [hoveredEmotion, setHoveredEmotion] = useState(''); 
-
   const handleMouseEnter = (emotion) => {
-    setHoveredEmotion(emotion);
-
     setEmotionImages((prevImages) => ({
       ...prevImages,
-      [emotion]: getHoverImage(emotion), 
+      [emotion]: getHoverImage(emotion),
     }));
   };
 
-  
   const handleMouseLeave = (emotion) => {
-    setHoveredEmotion('');
     setEmotionImages((prevImages) => ({
       ...prevImages,
-      [emotion]: getRegularImage(emotion), 
+      [emotion]: getRegularImage(emotion),
     }));
   };
 
- 
   const getRegularImage = (emotion) => {
     switch (emotion) {
       case 'excited':
@@ -54,11 +47,10 @@ function Header() {
       case 'upset':
         return upsetImg;
       default:
-        return excitedImg; 
+        return excitedImg;
     }
   };
 
- 
   const getHoverImage = (emotion) => {
     switch (emotion) {
       case 'excited':
@@ -72,97 +64,40 @@ function Header() {
       case 'upset':
         return upsetHoverImg;
       default:
-        return excitedHoverImg; 
+        return excitedHoverImg;
     }
   };
 
-  
-
   return (
-    <>
-
-      <div className="background-img" style={{ 
-        backgroundImage: 'url(src/assets/meadow-transparency.svg)',    
-       }}>
-
-        <h1 className='prompt'>How are you?</h1>
-
-        <div className='puppies-container'>
-
-          <a href="emotions/upset">
-          <div className="emotion-item">
-            <img
-              src={emotionImages.upset}
-              onMouseEnter={() => handleMouseEnter('upset')}
-              onMouseLeave={() => handleMouseLeave('upset')}
-              alt="Upset"
-              title={hoveredEmotion === 'upset' ? 'Upset' : ''} 
-            />
-             <p className="emotion-text">upset</p>
-          </div>
+    <div
+      className="background-img"
+      style={{
+        backgroundImage: 'url(src/assets/meadow-transparency.svg)',
+      }}
+    >
+      <h1 className="prompt">How are you?</h1>
+      <div className="puppies-container">
+        {['upset', 'tired', 'excited', 'proud', 'stressed'].map((emotion) => (
+          <a href={`emotions/${emotion}`} key={emotion}>
+            <div className="emotion-item">
+              <img
+                src={emotionImages[emotion]}
+                onMouseEnter={() => handleMouseEnter(emotion)}
+                onMouseLeave={() => handleMouseLeave(emotion)}
+                alt={emotion}
+                title={`I am ${emotion}`}
+              />
+              <p className="emotion-text">{emotion}</p>
+            </div>
           </a>
-
-          <a href="emotions/tired">
-          <div className="emotion-item">
-            <img
-              src={emotionImages.tired}
-              onMouseEnter={() => handleMouseEnter('tired')}
-              onMouseLeave={() => handleMouseLeave('tired')}
-              alt="Tired"
-              title={hoveredEmotion === 'tired' ? 'Tired' : ''} 
-            />
-            <p className="emotion-text">tired</p>
-          </div>
-          </a>
-
-
-
-          <a href="emotions/excited">
-          <div className="emotion-item">
-            <img
-              src={emotionImages.excited}
-              onMouseEnter={() => handleMouseEnter('excited')}
-              onMouseLeave={() => handleMouseLeave('excited')}
-              alt="Excited"
-              title={hoveredEmotion === 'excited' ? 'Excited' : ''} 
-            />
-            <p className="emotion-text">excited</p>
-          </div>
-          </a>
-
-          <a href="emotions/proud">
-          <div className="emotion-item">
-            <img
-              src={emotionImages.proud}
-              onMouseEnter={() => handleMouseEnter('proud')}
-              onMouseLeave={() => handleMouseLeave('proud')}
-              alt="Proud"
-              title={hoveredEmotion === 'proud' ? 'Proud' : ''} 
-            />
-            <p className="emotion-text">proud</p>
-          </div>
-          </a>
-
-          <a href="emotions/stressed">
-          <div className="emotion-item">
-            <img
-              src={emotionImages.stressed}
-              onMouseEnter={() => handleMouseEnter('stressed')}
-              onMouseLeave={() => handleMouseLeave('stressed')}
-              alt="Stressed"
-              title={hoveredEmotion === 'stressed' ? 'Stressed' : ''} 
-            />
-            <p className="emotion-text">stressed</p>
-          </div>
-          </a>
-          
-        </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
 export default Header;
+
 
 
 
